417c592283c15ee345bd4ec90d273159c5c9711d
WEI JUN <weijunext@gmail.com>
2023-11-24 14:11:05 +0800

    feat: Caterpillar

--- components/Caterpillar.js
+++ components/Caterpillar.js
@@ -7,7 +7,7 @@ export default function Caterpillar({ text, className }) {
   return (
     <div className={`flex ${className}`} onMouseLeave={() => { setSelected(null) }}>
       {chars.map((c, i) => (
-        <div key={i} className={`flex bg-indigo-600 ${i === 0 ? 'rounded-s-lg' : ''} ${i === chars.length - 1 ? 'rounded-e-lg' : ''} border-top border-bottom border-2 border-brand bg-primary text-background -ml-[2px] pb-[0.1rem] px-2 transition-transform ${selected === i ? '-translate-y-1' : ''}`} onMouseEnter={() => { setSelected(i) }}>
+        <div key={i} className={`flex bg-[#4472c4] ${i === 0 ? 'rounded-s-lg' : ''} ${i === chars.length - 1 ? 'rounded-e-lg' : ''} border-top border-bottom border-2 border-brand bg-primary text-background -ml-[2px] pb-[0.1rem] px-2 transition-transform ${selected === i ? '-translate-y-1' : ''}`} onMouseEnter={() => { setSelected(i) }}>
           <span>{c}</span>
         </div>
       ))}
