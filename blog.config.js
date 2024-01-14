63f1637e55d8af0f0abfc594829a84fdc09cc563
诸葛子房 <xiaoqiu2017wy@163.com>
2024-01-14 17:05:45 +0800

    fix:config info

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '诸葛子房', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '前京东、BAT程序员', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '前京东程序员', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zgzf.online', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '大数据, 用户画像', // 网站关键词 英文逗号隔开
 
