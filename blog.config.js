c760e5c2445b74153bfe24b2be4a699f6b1bb263
诸葛子房 <xiaoqiu2017wy@163.com>
2024-01-15 12:55:17 +0800

    fix:keyword

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '诸葛子房', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '前京东程序员', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zgzf.online', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '大数据, 用户画像', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '大数据,用户画像,Flink,Spark', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
